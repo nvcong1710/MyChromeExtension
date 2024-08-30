ca3a796109032a14d5886eed5fc82d766c3b4a68
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-08-30 16:02:30 +0700

    clean code

--- MyOnlineNote/popup.js
+++ MyOnlineNote/popup.js
@@ -4,10 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var noteSizeForm = document.getElementById("noteSizeForm");
   var widthInput = document.getElementById("widthInput");
   var heightInput = document.getElementById("heightInput");
-  var addNoteButton = document.getElementById("addNoteButton");
 
   noteSizeForm.addEventListener("submit", function (event) {
-    // event.preventDefault();
     var width = widthInput.value;
     var height = heightInput.value;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
